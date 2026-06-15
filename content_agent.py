736d495e649c4573cb22844f342ccdc5f660cd83
Shraddha Upadhye <anjalisantoshupadhye@Shraddha-Upadhye.local>
2026-06-15 21:51:47 +0530

    fixed groq deployment

--- content_agent.py
+++ content_agent.py
@@ -1,12 +1,16 @@
-from groq import Groq
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+import streamlit as st
+from groq import Groq
 
 load_dotenv()
 
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
+api_key = os.getenv("GROQ_API_KEY")
+
+if not api_key:
+    api_key = st.secrets["GROQ_API_KEY"]
+
+client = Groq(api_key=api_key)
 
 def generate_campaign_content(topic, location, date, time, venue):
 
